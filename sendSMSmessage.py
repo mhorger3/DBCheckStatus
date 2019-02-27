a43cb282eb298d1e16094c84e4040bb07ba3197f
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-02-26 22:22:57 -0500

    Create sendSMSmessage.py
    Add new lambda function that sends a SMS message based on IOT request

---
+++ sendSMSmessage.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+import boto3
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+sns = boto3.client('sns')
+phone_number = '1-484-949-1383'  # change it to your phone number
+
+
+def lambda_handler(event, context):
+    logger.info('Received event: ' + json.dumps(event))
+
+    #rds settings
+    rds_host = "weatherdata.ccgbnu8qerao.us-east-1.rds.amazonaws.com"
+    name = "lambda"
+    password = "lambda"
+    db_name = "weatherview"
+
+    try:
+        conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    except:
+        logger.error("ERROR: Unexpected error: Could not connect to" + rds_host + " instance.")
+        sys.exit()
+
+    #check next DB, same credientials but different DB and host
+    rds_host = "transitviewdata.ccgbnu8qerao.us-east-1.rds.amazonaws.com"
+    db_name = "transitviewdata";
+
+    try:
+        conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    except:
+        logger.error("ERROR: Unexpected error: Could not connect to" + rds_host + " instance.")
+        sys.exit()
+
+    message = 'Hello from your AWS Console %s. Your databases are all up!'
+    sns.publish(PhoneNumber=phone_number, Message=message)
+    logger.info('SMS has been sent to ' + phone_number)
